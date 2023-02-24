f4a7151cf3267510acc8418846884300c3e3b05a
TheRathustra <Purple-Pleb@yandex.ru>
2023-02-24 16:15:45 +0300

    реализация сервиса статистики

--- stat-service/stats-server/src/main/java/ru/practicum/statsServer/repository/StatRepository.java
+++ stat-service/stats-server/src/main/java/ru/practicum/statsServer/repository/StatRepository.java
@@ -16,25 +16,25 @@ public interface StatRepository extends JpaRepository<Hit, Long> {
     @Query("SELECT h.app, h.uri, count(*) as hits from Hit h " +
             "where h.timestamp between :start and :end " +
             "group by h.app, h.uri " +
-            "order by hits asc")
+            "order by hits DESC")
     List<HitAnswer> getStatsBetweenDates(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("SELECT distinct (h.uri) as uri, h.app, count(*) as hits from Hit h " +
             "where h.timestamp between :start and :end " +
             "GROUP BY h.app, h.uri " +
-            "order by hits asc")
+            "order by hits DESC")
     List<HitAnswer> getStatsBetweenDatesUnique(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     @Query("SELECT h.app as app, h.uri as uri, COUNT(h.id) as hits from Hit h " +
             "where h.uri in :uris and h.timestamp between :start and :end " +
             "group by h.app, h.uri " +
-            "order by hits asc")
+            "order by hits DESC")
     List<HitAnswer> getStatsBetweenDatesUriInCollection(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
     @Query("SELECT distinct (h.uri) as uri, h.app, count(*) as hits from Hit h " +
             "where h.uri in :uris and h.timestamp between :start and :end " +
             "GROUP BY h.app, h.uri " +
-            "order by hits asc ")
+            "order by hits DESC")
     List<HitAnswer> getStatsBetweenDatesUriInCollectionUnique(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
 }
