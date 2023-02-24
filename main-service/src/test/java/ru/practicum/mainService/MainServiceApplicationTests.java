16b7658b4bc632281aeee88f168b530d92dc59de
TheRathustra <Purple-Pleb@yandex.ru>
2023-02-24 12:02:12 +0300

    реализация сервиса статистики

--- main-service/src/test/java/ru/practicum/mainService/MainServiceApplicationTests.java
+++ main-service/src/test/java/ru/practicum/mainService/MainServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package ru.practicum.mainService;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- stat-service/stats-server/src/test/java/ru/practicum/statsServer/StatsServerApplicationTests.java
+++ stat-service/stats-server/src/test/java/ru/practicum/statsServer/StatsServerApplicationTests.java
@@ -1,6 +1,5 @@
 package ru.practicum.statsServer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
