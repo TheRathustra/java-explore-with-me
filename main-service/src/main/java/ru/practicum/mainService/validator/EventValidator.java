addd96556dce3582dc15841ff5e1465f2ed32190
TheRathustra <Purple-Pleb@yandex.ru>
2023-03-09 23:22:32 +0300

    реализация основного сервиса

--- main-service/src/main/java/ru/practicum/mainService/validator/EventValidator.java
+++ main-service/src/main/java/ru/practicum/mainService/validator/EventValidator.java
@@ -15,23 +15,29 @@ public class EventValidator {
     public void validateEvent(NewEventDto eventDto) {
 
         if (eventDto.getAnnotation() == null) {
-            throw new EventValidationException("Field: annotation. Error: Поле должно быть заполнено. Value: " + eventDto.getAnnotation());
+            throw new EventValidationException("Field: annotation. Error: Поле должно быть заполнено. " +
+                    "Value: " + eventDto.getAnnotation());
         }
 
         if (eventDto.getCategory() == null) {
-            throw new EventValidationException("Field: category. Error: Поле должно быть заполнено. Value: " + eventDto.getCategory());
+            throw new EventValidationException("Field: category. Error: Поле должно быть заполнено. " +
+                    "Value: " + eventDto.getCategory());
         }
 
         if (eventDto.getLocation() == null) {
-            throw new EventValidationException("Field: location. Error: Поле должно быть заполнено. Value: " + eventDto.getLocation());
+            throw new EventValidationException("Field: location. Error: Поле должно быть заполнено. " +
+                    "Value: " + eventDto.getLocation());
         }
 
         if (eventDto.getTitle() == null) {
-            throw new EventValidationException("Field: title. Error: Поле должно быть заполнено. Value: " + eventDto.getTitle());
+            throw new EventValidationException("Field: title. Error: Поле должно быть заполнено. " +
+                    "Value: " + eventDto.getTitle());
         }
 
-        if (eventDto.getEventDate() == null || eventDto.getEventDate().isBefore(LocalDateTime.now().plus(2, ChronoUnit.HOURS))) {
-            throw new EventValidationException("Field: eventDate. Error: должно содержать дату, которая еще не наступила. Value: " + eventDto.getEventDate());
+        if (eventDto.getEventDate() == null || eventDto.getEventDate().isBefore(LocalDateTime.now()
+                .plus(2, ChronoUnit.HOURS))) {
+            throw new EventValidationException("Field: eventDate. Error: должно содержать дату, " +
+                    "которая еще не наступила. Value: " + eventDto.getEventDate());
         }
 
     }
@@ -40,9 +46,10 @@ public class EventValidator {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
-        LocalDateTime eventDate = LocalDateTime.parse(eventDto.getEventDate(), formatter);
-        if (eventDto.getEventDate() != null && eventDate.isBefore(LocalDateTime.now().plus(2, ChronoUnit.HOURS))) {
-            throw new EventValidationException("Field: eventDate. Error: должно содержать дату, которая еще не наступила. Value: " + eventDto.getEventDate());
+        if (eventDto.getEventDate() != null && LocalDateTime.parse(eventDto.getEventDate(), formatter)
+                .isBefore(LocalDateTime.now().plus(2, ChronoUnit.HOURS))) {
+            throw new EventValidationException("Field: eventDate. Error: должно содержать дату, " +
+                    "которая еще не наступила. Value: " + eventDto.getEventDate());
         }
     }
 
